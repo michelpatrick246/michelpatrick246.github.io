34ef1f8bef15237083c3edf521c0a19d1423f885
Michel Patrick <michelpatrick246@gmail.com>
2025-07-13 20:24:07 +0300

    Fix: use HTTPS base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: "/michelpatrick246.github.io/"
+  base: "/"
 });
